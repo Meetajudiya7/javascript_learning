7acf2190caeb1e6152ada3ce6d85e647a7ffdd81
Meetajudiya7 <meetajudiya123@gmail.com>
2025-11-27 06:39:58 +0000

    container config done

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,30 @@
+"use strict"; //treat all JS code as newer version
+
+//alert(3+3);
+
+console.log(3 + 3); // code readability should be high
+console.log("hello");
+
+let name = "meet";
+let age= 20;
+let isLoggedIn = false;
+let state;
+
+
+//number => 2 to power 53
+//bignint
+//string => ""
+//boolean => true/false
+//null => standalone value
+//undefined=>
+//symbol => unique
+
+
+//object
+
+console.log(typeof age);
+console.log(typeof undefined);
+console.log(typeof null);
+
+
+
