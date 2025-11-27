42f191c316107fce18ef554cd156cf6e01797363
Meetajudiya7 <meetajudiya123@gmail.com>
2025-11-27 09:46:01 +0000

    container config done

--- 01_basics/06_datatypessummry.js
+++ 01_basics/06_datatypessummry.js
@@ -1,45 +1,67 @@
-//Value Type (Primitive)
+// //Value Type (Primitive)
 
-// 7 types : String, Number, Boolean, null,undefined, Symbol, BigInt
+// // 7 types : String, Number, Boolean, null,undefined, Symbol, BigInt
 
-const score = 100;
-const scoreValue = 100.3;
-const isLoggedIn = false;
-const outsideTemp = null;
-let userEmail; // By default undifined
+// const score = 100;
+// const scoreValue = 100.3;
+// const isLoggedIn = false;
+// const outsideTemp = null;
+// let userEmail; // By default undifined
 
-console.log(typeof scoreValue);
+// console.log(typeof scoreValue);
 
-const id = Symbol("123");
-const anotherId = Symbol("123");
+// const id = Symbol("123");
+// const anotherId = Symbol("123");
 
-console.log(typeof anotherId);
+// console.log(typeof anotherId);
+
+// console.log(id === anotherId);
+
+// const bigNumber = 321231156123n;
+// console.log(typeof bigNumber);
+
+// // Reference types (Non Primitive)
+
+// // Array, Objects, Functions
+
+// const heros = ["shaktiman", "naagraj", "superman"];
+// console.log(typeof heros);
+
+// let myObj = {
+//   name: "meet",
+//   age: 20,
+// };
+
+// console.log(typeof myObj);
+
+// const myfunction=function(){
+//     console.log("hello wolrd");
+// }
+
+// console.log(typeof myfunction);
+
+//  **************************************************88
+
+// stack(primitive), Heap (Non-primitive)
+
+let myname = "meetajudiya";
+
+let anothername = myname;
+anothername = "meetnarola";
 
 
-console.log(id === anotherId);
+console.log(myname);
+console.log(anothername);
 
-const bigNumber = 321231156123n;
-console.log(typeof bigNumber);
-
-
-// Reference types (Non Primitive)
-
-// Array, Objects, Functions
-
-const heros = ["shaktiman", "naagraj", "superman"];
-console.log(typeof heros);
-
-let myObj = {
-  name: "meet",
-  age: 20,
-};
-
-console.log(typeof myObj);
-
-const myfunction=function(){
-    console.log("hello wolrd");
+let userone={
+    email:"abc@gmail.com",
+    upi:"user@ybl"
 }
 
-console.log(typeof myfunction);
+let usertwo=userone
 
+usertwo.email="xyz@gmail.com"
+
+console.log(userone.email);
+console.log(usertwo.email);
 
