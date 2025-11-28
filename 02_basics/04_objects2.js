74653c18a581dea8d58c719646343c0eed8baf50
Meetajudiya7 <meetajudiya123@gmail.com>
2025-11-28 10:13:31 +0000

    container config done

--- 02_basics/04_objects2.js
+++ 02_basics/04_objects2.js
@@ -1,56 +1,67 @@
-// const tinder = new Object()//singleton
+// // const tinder = new Object()//singleton
 
-const tineruser = {} // Non Singleton
+// const tineruser = {} // Non Singleton
 
-tineruser.id = "123abc"
-tineruser.name = "sam"
-tineruser.isLoggedIn = false
+// tineruser.id = "123abc"
+// tineruser.name = "sam"
+// tineruser.isLoggedIn = false
 
 
-const regularUser = {
-    email: "some@gmail.com",
-    fullname: {
-        userfullname: {
-            firstname: "sam",
-            lastname: "choudhary"
+// const regularUser = {
+//     email: "some@gmail.com",
+//     fullname: {
+//         userfullname: {
+//             firstname: "sam",
+//             lastname: "choudhary"
 
-        }
-    }
+//         }
+//     }
+// }
+
+
+// // console.log(regularUser.fullname.userfullname.firstname); // Nested Objects data access this style
+
+
+// const obj1 = { 1: "a", 2: "b" }
+// const obj2 = { 3: "c", 4: "d" }
+// const obj4 = { 5: "c", 6: "d" }
+
+// // const obj3 = {obj1,obj2 }
+
+// // const obj3=Object.assign({},obj1,obj2,obj4)
+
+// const obj3 = { ...obj1, ...obj2 }// Most Usable
+// console.log(obj3);
+
+
+// const users = [{
+//     id: 1,
+//     email: "nenish@gmail.com"
+// },
+// {
+//     id: 1,
+//     email: "nenish@gmail.com"
+// }
+// ] //when database through value fetch that time use this method
+
+// users[1].email
+// console.log(tineruser);
+// console.log(Object.keys(tineruser));
+// console.log(Object.values(tineruser));
+// console.log(Object.entries(tineruser));
+// console.log(tineruser.hasOwnProperty('isLoggedIn'));//check the property available or not
+
+
+const course={
+    courename:"js in hindi",
+    price:"999",
+    courseInstructor:"hitesh"
 }
 
+// course.courseInstructor
 
-// console.log(regularUser.fullname.userfullname.firstname); // Nested Objects data access this style
+const {courseInstructor: instructor}=course
 
+console.log(instructor);
 
-const obj1 = { 1: "a", 2: "b" }
-const obj2 = { 3: "c", 4: "d" }
-const obj4 = { 5: "c", 6: "d" }
-
-// const obj3 = {obj1,obj2 }
-
-// const obj3=Object.assign({},obj1,obj2,obj4)
-
-const obj3 = { ...obj1, ...obj2 }// Most Usable
-console.log(obj3);
-
-
-const users = [{
-    id: 1,
-    email: "nenish@gmail.com"
-},
-{
-    id: 1,
-    email: "nenish@gmail.com"
-}
-] //when database through value fetch that time use this method
-
-users[1].email
-console.log(tineruser);
-console.log(Object.keys(tineruser));
-console.log(Object.values(tineruser));
-console.log(Object.entries(tineruser));
-console.log(tineruser.hasOwnProperty('isLoggedIn'));//check the property available or not
-
-
-
-
+ 
