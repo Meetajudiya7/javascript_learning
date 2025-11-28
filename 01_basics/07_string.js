63e9c0d9591e765c7d2503cf568581fcbc937bf4
Meetajudiya7 <meetajudiya123@gmail.com>
2025-11-28 06:08:08 +0000

    container config done

--- 01_basics/07_string.js
+++ 01_basics/07_string.js
@@ -1,11 +1,11 @@
-const name="meet"
-const repoCount=50
+const name = "meet"
+const repoCount = 50
 
 // console.log(name+repoCount+"value");
 
 console.log(`Hello my name is${name} and my repo count is ${repoCount}`);
 
-const gamename= new String('meet-fc')
+const gamename = new String('meet-fc')
 
 console.log(gamename[0]);
 console.log(gamename.__proto__);
@@ -14,27 +14,27 @@ console.log(gamename.toUpperCase());
 console.log(gamename.charAt(2));
 console.log(gamename.indexOf('t'));
 
-const newString=gamename.substring(0,4)
+const newString = gamename.substring(0, 4)
 console.log(newString);
 
 
 
-const anotherstring = gamename.slice(-5,4)
+const anotherstring = gamename.slice(-5, 4)
 console.log(anotherstring);
 
-const newstringone="  meet  "
+const newstringone = "  meet  "
 console.log(newstringone);
 console.log(newstringone.trim());
 
-const url="https://google.com/google%20chrome"
+const url = "https://google.com/google%20chrome"
 
-console.log(url.replace('%20','-'));
+console.log(url.replace('%20', '-'));
 
 console.log(url.includes('google'));
-console.log(gamename.split('-') );
+console.log(gamename.split('-'));
 
 
-  
+
 
 
 
