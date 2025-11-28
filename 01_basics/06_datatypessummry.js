9e6df8f5e500c27e208f8a9c1a13da3b3362a5ef
Meetajudiya7 <meetajudiya123@gmail.com>
2025-11-28 09:03:58 +0000

    container config done

--- 01_basics/06_datatypessummry.js
+++ 01_basics/06_datatypessummry.js
@@ -2,41 +2,41 @@
 
 // // 7 types : String, Number, Boolean, null,undefined, Symbol, BigInt
 
-// const score = 100;
-// const scoreValue = 100.3;
-// const isLoggedIn = false;
-// const outsideTemp = null;
-// let userEmail; // By default undifined
+const score = 100;
+const scoreValue = 100.3;
+const isLoggedIn = false;
+const outsideTemp = null;
+let userEmail; // By default undifined
 
-// console.log(typeof scoreValue);
+console.log(typeof scoreValue);
 
-// const id = Symbol("123");
-// const anotherId = Symbol("123");
+const id = Symbol("123");
+const anotherId = Symbol("123");
 
-// console.log(typeof anotherId);
+console.log(typeof anotherId);
 
-// console.log(id === anotherId);
+console.log(id === anotherId);
 
-// const bigNumber = 321231156123n;
-// console.log(typeof bigNumber);
+const bigNumber = 321231156123n;
+console.log(typeof bigNumber);
 
-// // Reference types (Non Primitive)
+// Reference types (Non Primitive)
 
-// // Array, Objects, Functions
+// Array, Objects, Functions
 
-// const heros = ["shaktiman", "naagraj", "superman"];
-// console.log(typeof heros);
+const heros = ["shaktiman", "naagraj", "superman"];
+console.log(typeof heros);
 
-// let myObj = {
-//   name: "meet",
-//   age: 20,
-// };
+let myObj = {
+  name: "meet",
+  age: 20,
+};
 
-// console.log(typeof myObj);
+console.log(typeof myObj);
 
-// const myfunction=function(){
-//     console.log("hello wolrd");
-// }
+const myfunction=function(){
+    console.log("hello wolrd");
+}
 
 // console.log(typeof myfunction);
 
