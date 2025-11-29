5d3b6184a7878e6fb56739b4273b89cd7f934df5
Meetajudiya7 <meetajudiya123@gmail.com>
2025-11-29 08:53:03 +0000

    container config done

---
+++ 05_loops/02_while_dowhile.js
@@ -0,0 +1,28 @@
+// while 
+
+let index = 0
+while (index <= 10) {
+
+    console.log(`value of index is ${index}`);
+    index = index + 2
+
+
+}
+
+let myarray = ['falsh', 'batman', 'superman']
+
+let arr = 0
+
+while (arr < myarray.length) {
+    console.log(`Value  is ${myarray[arr]}`);
+    arr = arr + 1
+}
+
+//do while loop
+
+let score = 1
+
+do {
+    console.log(`score is ${score}`)
+    score++
+} while (score <= 10);
