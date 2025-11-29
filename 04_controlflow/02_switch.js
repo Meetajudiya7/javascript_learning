57a692e1da6e4ca0118f3994292f0018d716e54a
Meetajudiya7 <meetajudiya123@gmail.com>
2025-11-29 06:05:03 +0000

    container config done

---
+++ 04_controlflow/02_switch.js
@@ -0,0 +1,57 @@
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+const month=3
+
+switch (3) {
+    case 1:
+        console.log("january");
+        break;
+    case 2:
+        console.log("feb");
+        break;
+    case 3:
+        console.log("mar");
+        break;
+    case 4:
+        console.log("april");
+        break;
+    case 5:
+        console.log("may");
+        break;
+
+    default:
+        console.log("do not match");
+        
+        break;
+}
+
+const month1="mar"
+
+switch ("mar") {
+    case "jan":
+        console.log("january");
+        break;
+    case "feb":
+        console.log("feb");
+        break;
+    case "mar":
+        console.log("mar");
+        break;
+    case "apr":
+        console.log("april");
+        break;
+    case "may":
+        console.log("may");
+        break;
+
+    default:
+        console.log("do not match");
+        break;
+}
